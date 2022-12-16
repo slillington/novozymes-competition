15577d58b499b2c31696b99d9d3e24243b45e709
My Nguyen <nvthaomy@yahoo.com>
2022-12-16 10:45:23 -0800

    add 2 more clusters

--- regression/combine_datasets_ph.py
+++ regression/combine_datasets_ph.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 ph_target = 7.5
-ph_tol = 0.5
-exclude_set = '21069'
+ph_tol = 1.0
+exclude_set = '972'
 name = 'dataset_combined_exclude{}'.format(exclude_set)
 
-data_tags = ['972', '13268', '17481', '5134', '24305', '14603', '16540', '17775', '17926', '21069', '22093', '26901', '18020']
-i_wts = [21, 90, 38, 38, 90, 28, 172, 25, 41, 1, 46, 80, 84]
+data_tags = ['972', '13268', '17481', '5134', '24305', '14603', '16540', '17775', '17926', '21069', '22093', '26901', '18020', '13754','9834']
+i_wts = [21, 90, 38, 38, 90, 28, 172, 25, 41, 1, 46, 80, 84, 0, 0]
+# number of member after filtering: 100 0 0 20 144 46 254 86 51 78 49 0 109 84 7
 
 for i in range(len(data_tags)):
     this_dataset = np.loadtxt('datasets_cleaned/' + data_tags[i] + '_data_clean.csv', delimiter=',', skiprows=1)
@@ -24,6 +25,6 @@ for i in range(len(data_tags)):
             dataset = np.vstack((dataset, this_dataset))
 
     #save individual dataset:     
-    np.savetxt('datasets_ph/{}_ph{}.csv'.format(data_tags[i], ph_target), this_dataset, delimiter=',', header='delta: tm,sasa,charge,volume,helix,sheet,coil,n_phobic_contact,n_disulfide,n_saltbridge,n_hbond,n_pos-pos,n_pos-neutral,n_pos-special,n_pos-hydrophobic,n_neg-neg,n_neg-neutral,n_neg-special,n_neg-hydrophobic,n_neutral-neutral,n_neutral-special,n_neutral-hydrophobic,n_special-special,n_special-hydrophobic')
+    np.savetxt('datasets_ph/{}_ph{}_tol{}.csv'.format(data_tags[i], ph_target, ph_tol), this_dataset, delimiter=',', header='delta: tm,sasa,charge,volume,helix,sheet,coil,n_phobic_contact,n_disulfide,n_saltbridge,n_hbond,n_pos-pos,n_pos-neutral,n_pos-special,n_pos-hydrophobic,n_neg-neg,n_neg-neutral,n_neg-special,n_neg-hydrophobic,n_neutral-neutral,n_neutral-special,n_neutral-hydrophobic,n_special-special,n_special-hydrophobic')
 
-np.savetxt('{}_ph{}.csv'.format(name, ph_target), dataset, delimiter=',', header='delta: tm,sasa,charge,volume,helix,sheet,coil,n_phobic_contact,n_disulfide,n_saltbridge,n_hbond,n_pos-pos,n_pos-neutral,n_pos-special,n_pos-hydrophobic,n_neg-neg,n_neg-neutral,n_neg-special,n_neg-hydrophobic,n_neutral-neutral,n_neutral-special,n_neutral-hydrophobic,n_special-special,n_special-hydrophobic')
+np.savetxt('{}_ph{}_tol{}.csv'.format(name, ph_target, ph_tol), dataset, delimiter=',', header='delta: tm,sasa,charge,volume,helix,sheet,coil,n_phobic_contact,n_disulfide,n_saltbridge,n_hbond,n_pos-pos,n_pos-neutral,n_pos-special,n_pos-hydrophobic,n_neg-neg,n_neg-neutral,n_neg-special,n_neg-hydrophobic,n_neutral-neutral,n_neutral-special,n_neutral-hydrophobic,n_special-special,n_special-hydrophobic')
